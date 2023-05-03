f91ccbc5da063ca0c245c55e2ec321359906ca29
Ivan Duarte <ivangermanoduarte@gmail.com>
2023-05-03 12:10:55 -0300

    Alterações no index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
-// import { v4 as uuidV4 } from "uuid"
+import { v4 as uuidV4 } from "uuid"
 
-// console.log(uuidV4())
+type Task = {                //Crio tipo com o nome de "Task" e atribuo os parametros que são atribuidos a cont task
+    id: string, 
+    title: string, 
+    completed: boolean, 
+    createdAt: Date
+}
 
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("new-task-form") as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
+form?.addEventListener("submit", e => {
+    e.preventDefault()
+
+    if (input?.value == "" || input?.value == null) return
+
+    const newTask: Task = {   //Crio um objeto do tipo "Task" definido no inicio do código
+        id: uuidV4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date()
+    }
+    input.value
+
+    addListItem(newTask)
+})
+//Chamo o tipo "Task" que criei para a função de adicionar um item na lista
+function addListItem(task: Task) {} 
